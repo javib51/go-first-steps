108d0a9a1b83f2138b3cfb9dcbdd1cad54a6ae78
Javier Benitez <javier.benitezf51@gmail.com>
2016-11-26 15:34:16 +0100

    Under development parallel mergesort

--- algorithms/mergesort.go
+++ algorithms/mergesort.go
@@ -1,7 +1,9 @@
 package algorithms
-
+//import "runtime"
+//import "sync"
+/*
 // Merge two arrays
-func Merge (left, right []int) ([]int){
+func Merge(left, right []int) ([]int){
 	list := make([]int, len(left) + len(right))
 	i := 0
 	j := 0
@@ -33,12 +35,6 @@ func Merge (left, right []int) ([]int){
 	return list
 }
 
-// Merge two arrays in parallel
-func MergeP (left, right []int) ([]int){
-	//TODO
-	return Merge(left, right)
-}
-
 // Merge sort with no extra allocation
 func MergeSort(data []int) ([]int){
 	if len(data) > 1 {
@@ -49,23 +45,91 @@ func MergeSort(data []int) ([]int){
 	}
 	
 	return data
+}*/
+
+// Merge two arrays
+func Merge(list, left, right []int){
+	//list := make([]int, len(left) + len(right))
+	i := 0
+	j := 0
+	k := 0
+
+	for i < len(left) && j < len(right) {
+		if left[i] <= right[j] {
+			list[k] = left[i]
+			k++
+			i++
+		} else {
+			list[k] = right[j]
+			k++
+			j++
+		}
+	}
+
+	for i < len(left) {
+		list[k] = left[i]
+		k++
+		i++
+	}
+
+	for j < len(right) {
+		list[k] = right[j]
+		k++
+		j++
+	}
 }
 
-// Parallel Merge sort with no extra allocation
-func PMergeSort(data []int) ([]int){
-	//TODO
-	return MergeSort(data)
+// Merge sort with no extra allocation
+func MergeSort(data []int) ([]int){
+	if len(data) > 1 {
+		middle := len(data)/2
+		MergeSort(data[:middle])
+		MergeSort(data[middle:])
+		Merge(data, data[:middle], data[middle:])
+	}
+	return data
 }
+
+/*
+// Parallel Merge sort with no extra allocation
+func PMergeSort(data []int){
+	
+	var threads int = runtime.NumCPU()
+	var n int = len(data)
+	var block int = n/threads
+
+	//runtime.GOMAXPROCS(threads)
+
+	var wg sync.WaitGroup
+	
+	for i:=0; i < threads; i++ {
+		wg.Add(1)
+		go func( pos, b int, data []int){
+			start := pos*b
+			end := pos*b + b
+			QuickSort(data[start:end])
+			wg.Done()
+		}(i, block, data)
+	}
+	wg.Wait()
+
+	for i:=threads; i > 1; i*=0.5 {
+		block = n/i
+		
+	}
+	
+	
+}*/
 
 
 
 // Merge sort with no extra allocation
-func MergeSortFast(data []int){
+func MergeSortNoAlloc(data []int){
 	if len(data) > 1 {
 		middle := len(data)/2
-		MergeSortFast(data[:middle])
-		MergeSortFast(data[middle:])
+		MergeSortNoAlloc(data[:middle])
+		MergeSortNoAlloc(data[middle:])
 		QuickSort(data)
-		//MergeFast(data, middle)
+		//MergeNoAlloc(data, middle)
 	}
 }
